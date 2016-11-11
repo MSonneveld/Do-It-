894896db98d25e2ff8af736f88fdceb5c23c0bb5
Mickey Sonneveld <mickeysonneveld@Mickeys-MacBook-Pro.local>
2016-11-11 14:06:57 +0100

    Do It! first commit

---
+++ Do It!/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  Do It!
+//
+//  Created by Mickey Sonneveld on 11/11/16.
+//  Copyright © 2016 Mickey Sonneveld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tasks = makeTasks()
+        
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        }
+        else{
+            
+            cell.textLabel?.text = task.name
+        }
+                return cell
+    }
+
+    
+    func makeTasks() -> [Task]{
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "mow the lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
